5c479138c5b61a9b7e8071db1fbbc287d6c631d5
voiys <matej2714@gmail.com>
2023-11-22 17:41:38 +0100

    fix solutions

--- 5_group_by/solutions.sql
+++ 5_group_by/solutions.sql
@@ -1,14 +1,14 @@
 -- exercise 1
 SELECT GenreId, COUNT(TrackId) AS TrackCount
-FROM Tracks
+FROM Track
 GROUP BY GenreId;
 
 -- exercise 2
 SELECT BillingCountry, SUM(Total) AS TotalSales
-FROM Invoices
+FROM Invoice
 GROUP BY BillingCountry;
 
 -- exercise 3
 SELECT AlbumId, AVG(Milliseconds) AS AverageLength
-FROM Tracks
+FROM Track
 GROUP BY AlbumId;
