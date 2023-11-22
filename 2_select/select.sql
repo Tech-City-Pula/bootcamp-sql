0f3e7a4e9852184713364c2eac0d38998ef49027
voiys <matej2714@gmail.com>
2023-11-22 17:09:36 +0100

    update example

--- 2_select/select.sql
+++ 2_select/select.sql
@@ -1,4 +1,10 @@
+
+-- select all columns from the Artists table
 SELECT * FROM Artist;
 
+-- select all columns from the Album table for the artist with an ArtistID of 1
 SELECT * FROM Album
 WHERE ArtistID = 1;
+
+-- select only the Name column from the Artist table
+SELECT Name FROM Artist;
