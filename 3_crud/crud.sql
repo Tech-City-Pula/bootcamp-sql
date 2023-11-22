de42aebb61067bbab151c66b88356a758cb23061
voiys <matej2714@gmail.com>
2023-11-22 17:10:00 +0100

    update example

--- 3_crud/crud.sql
+++ 3_crud/crud.sql
@@ -1,9 +1,12 @@
+-- insert new row
 INSERT INTO Album (Title, ArtistID)
 VALUES ('The Dark Side of the Moon', 1);
 
+-- update existing row
 UPDATE Album
 SET ArtistID = 120
 WHERE Title = 'The Dark Side of the Moon';
 
+-- delete row
 DELETE FROM Album
 WHERE Title = 'The Dark Side of the Moon';
