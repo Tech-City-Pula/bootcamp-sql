83bc54962d08d3f2f438c511bc16223082d5c146
voiys <matej2714@gmail.com>
2023-11-22 17:39:37 +0100

    fix solutions

--- 2_select/solutions.sql
+++ 2_select/solutions.sql
@@ -1,14 +1,14 @@
 -- exercise 1
-SELECT * FROM Tracks WHERE Composer = 'Queen';
+SELECT * FROM Track WHERE Composer = 'Queen';
 
 -- exercise 2
-SELECT Title, AlbumId FROM Albums WHERE ArtistId = 22;
+SELECT Title, AlbumId FROM Album WHERE ArtistId = 22;
 
 -- exercise 3
-SELECT TrackId, Name, UnitPrice FROM Tracks WHERE UnitPrice > 0.99;
+SELECT TrackId, Name, UnitPrice FROM Track WHERE UnitPrice > 0.99;
 
 -- exercise 4
-SELECT * FROM Customers WHERE FirstName LIKE 'A%';
+SELECT * FROM Customer WHERE FirstName LIKE 'A%';
 
 -- exercise 5
-SELECT TrackId, Name, GenreId FROM Tracks WHERE GenreId = 1 OR UnitPrice < 0.99;
+SELECT TrackId, Name, GenreId FROM Track WHERE GenreId = 1 OR UnitPrice < 0.99;
