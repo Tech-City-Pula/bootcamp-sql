2f6598e5b92abe83e9af7bfbbd1682f8f9719258
voiys <matej2714@gmail.com>
2023-11-22 17:41:05 +0100

    fix solutions

--- 4_order_by/solutions.sql
+++ 4_order_by/solutions.sql
@@ -1,13 +1,13 @@
 -- exercise 1
-SELECT * FROM Customers
+SELECT * FROM Customer
 ORDER BY Country;
 
 -- exercise 2
 SELECT Name, Milliseconds
-FROM Tracks
+FROM Track
 ORDER BY Milliseconds DESC;
 
 -- exercise 3
 SELECT AlbumId, Title, ArtistId
-FROM Albums
+FROM Album
 ORDER BY Title ASC, ArtistId DESC;
